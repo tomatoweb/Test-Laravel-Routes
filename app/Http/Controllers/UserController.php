a5f676c58ea8d0d7e60e296f8223b0e950e5f0ec
Mathias Appelmans <math.appelmans@gmail.com>
2025-03-28 13:32:02 +0100

    added response code to the features phpunit tests

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ class UserController extends Controller
     public function show($name)
     {
         $user = User::where('name', $name)->first();
+        dd($user);
         if (!$user) {
             return view('users.notfound');
         }
